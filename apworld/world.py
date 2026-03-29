6329de03752edcac21d1f561019d5d08ff4eecf2
no-comma <82644961+no-comma@users.noreply.github.com>
2026-03-29 13:17:24 +0300

    Use dict comprehensions

--- apworld/world.py
+++ apworld/world.py
@@ -24,14 +24,8 @@ class FungerWorld(World):
     options_dataclass = options.FungerOptions
     options: options.FungerOptions
 
-    location_name_to_id = {}
-    for region in locations.LOCATIONS:
-        for key, value in locations.LOCATIONS[region].items():
-            location_name_to_id[key] = value
-
-    item_name_to_id = {}
-    for item_name, item in items.ITEMS.items():
-        item_name_to_id[item_name] = item[1]
+    location_name_to_id = {name: id for region in locations.LOCATIONS.values() for name, id in region.items()}
+    item_name_to_id = {name: item[1] for name, item in items.ITEMS.items()}
 
     # There is always one region that the generator starts from & assumes you can always go back to.
     # This defaults to "Menu", but you can change it by overriding origin_region_name.
